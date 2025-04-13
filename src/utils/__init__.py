5c4ab0b1e47d8862ed0429a6ff96226438059c21
snoozleEmily <emilyalvimicm@gmail.com>
2025-04-13 15:10:27 -0300

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,13 +1,11 @@
 from .audio_processor import check_ffmpeg, extract_audio, clean_audio
 from .transcriber import Transcriber
 from .file_handler import save_transcription
-from .status import Status
 
 __all__ = [
     "check_ffmpeg",
     "extract_audio",
     "clean_audio",
     "Transcriber",
-    "save_transcription",
-    "Status"
+    "save_transcription"
 ]
