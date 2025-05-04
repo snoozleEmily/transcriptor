453647c2cbe5b5410052e93628f948cecd90299b
snoozleEmily <emilyalvimicm@gmail.com>
2025-05-04 11:44:34 -0300

    Update __init__.py

--- src/frontend/__init__.py
+++ src/frontend/__init__.py
@@ -2,6 +2,7 @@ from .constants import THEMES, FONTS
 from .interface import Interface
 from .theme import configure_theme
 from .url_opener import open_browser
+from .warning_popup import show_popup
 from .async_processor import AsyncTaskManager
 from .styles_manager import StyleManager
 from .widgets import Header, ButtonsPanel, MainWindow
