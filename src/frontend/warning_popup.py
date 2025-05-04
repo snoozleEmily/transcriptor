fe76f5ca4eb4b4b36fb5f90a045497c76f7d7a5c
snoozleEmily <emilyalvimicm@gmail.com>
2025-05-04 11:44:28 -0300

    Create warning_popup.py

---
+++ src/frontend/warning_popup.py
@@ -0,0 +1,2 @@
+def show_popup():
+    raise NotImplementedError("You still need to create this\nexplaniing how the app is still in trial and AI is used,\nso the end result should not be 100% trusted")
