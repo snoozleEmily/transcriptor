d7257917e983d73e8ee07a88ac29a9cbe891ce9d
snoozleEmily <emilyalvimicm@gmail.com>
2025-06-29 18:17:04 -0300

    Update metadata.py

--- config/metadata.py
+++ config/metadata.py
@@ -3,7 +3,7 @@ from __url__ import __url__
 
 
 NAME = "emilys-transcriptor"
-DESCRIPTION = "Turn videos into text and get organized notes from lengthy videos - works offline!"
+DESCRIPTION = "Turn videos into text and get organized notes from lengthy videos. It works offline!"
 LICENSE = "GPLv3"
 KEYWORDS = ["transcription", "audio", "video", "whisper", "notes", "summarization"]
 PROJECT_URLS = __url__
