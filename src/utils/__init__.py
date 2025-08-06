53a033a4f5848c26314b6b590aba70ec1daff686
snoozleEmily <emilyalvimicm@gmail.com>
2025-08-05 21:08:04 -0300

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,17 +1,19 @@
-from .audio_processor import check_ffmpeg, extract_audio, clean_audio
-from .file_handler import save_transcription
-from .transcripting.textify import Textify
-from .end_flow import EndFlow
+from src.utils.audio_processor import check_ffmpeg, extract_audio, clean_audio
+from src.utils.file_handler import save_transcription
+from src.utils.pdf_maker import PDFExporter
+from src.utils.transcripting.textify import Textify
+from src.utils.end_flow import EndFlow
 from src.utils.text.language import Language
 from src.utils.text.content_type import ContentType
-from .models import MODELS, MODEL_SPEEDS, SETUP_TIMES
+from src.utils.models import MODELS, MODEL_SPEEDS, SETUP_TIMES
 
 __all__ = [
     "check_ffmpeg",
     "extract_audio",
     "clean_audio",
     "Textify",
-    "save_transcription"
+    "save_transcription",
+    "PDFExporter",
     "EndFlow",
     "ContentType",
     "MODELS", 
