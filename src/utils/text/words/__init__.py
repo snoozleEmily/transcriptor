52563f18a01eee1ec1f3aa76f2a3498501074980
snoozleEmily <emilyalvimicm@gmail.com>
2025-08-10 20:10:40 -0300

    Create __init__.py

---
+++ src/utils/text/words/__init__.py
@@ -0,0 +1,9 @@
+from src.utils.text.words.question import QUESTION_WRD 
+from src.utils.text.words.definition_pat import DEFINITION_PAT
+from src.utils.text.words.common import COMMON_WORDS
+
+__all__ = [
+    "QUESTION_WRD",
+    "DEFINITION_PAT",
+    "COMMON_WORDS"
+]
