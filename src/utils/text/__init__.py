fcca50844598237495cd27d400e2c1e4f599d53a
snoozleEmily <emilyalvimicm@gmail.com>
2025-08-10 20:10:38 -0300

    Update __init__.py

--- src/utils/text/__init__.py
+++ src/utils/text/__init__.py
@@ -1,12 +1,9 @@
-from src.utils.text.word_snippets import QUESTION_WRD, DEFINITION_PAT
 from src.utils.text.content_type import ContentType
 from src.utils.text.notes_generator import NotesGenerator
 from src.utils.text.text_reviser import TextReviser
 from src.utils.text.language import Language
 
 __all__ = [
-    "QUESTION_WRD",
-    "DEFINITION_PAT",
     "ContentType",
     "NotesGenerator",
     "TextReviser",
