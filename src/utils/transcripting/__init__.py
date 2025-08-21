bec5b7c4f8b12c77c7ad1b26c73a86d3dda5d945
snoozleEmily <emilyalvimicm@gmail.com>
2025-08-21 13:41:20 -0300

    Update __init__.py

--- src/utils/transcripting/__init__.py
+++ src/utils/transcripting/__init__.py
@@ -2,14 +2,14 @@ from .textify import Textify
 from .loader import Loader
 from .info_dump import InfoDump
 from .convert_audio import ConvertAudio
-from .output_content import OutputContent
+from .sanitize_prompt import SanitizePrompt
 from .set_model import SetModel
 from .estimator import TimeEstimator
 
 __all__ = [
     "Textify",
     "ConvertAudio",
-    "OutputContent",
+    "SanitizePrompt",
     "SetModel",
     "InfoDump",
     "TimeEstimator",
