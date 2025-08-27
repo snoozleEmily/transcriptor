559d29cf841353ab3d9f003e46cfe8454af44263
snoozleEmily <emilyalvimicm@gmail.com>
2025-08-27 20:01:13 -0300

    Update set_model.py

--- src/utils/transcripting/set_model.py
+++ src/utils/transcripting/set_model.py
@@ -1,6 +1,7 @@
 import whisper
 
 
+from src.errors.debug import debug
 from src.utils.models import MODELS
 from src.errors.exceptions import TranscriptionError
 
@@ -9,11 +10,15 @@ from src.errors.exceptions import TranscriptionError
 class SetModel:
     """Manages Whisper model loading"""
     def load(self, model_size: str):
+        debug.dprint(f"Requested Whisper model: {model_size}")
+
         if model_size not in MODELS:
             raise TranscriptionError.invalid_model()
             
         try:
-            return whisper.load_model(model_size)
+            model = whisper.load_model(model_size)
+            debug.dprint(f"Successfully loaded model: {model_size} of type: {type(model)}")
+            return model
         
         except Exception as e:
             raise TranscriptionError.load_failed() from e
