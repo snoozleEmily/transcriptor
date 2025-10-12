f6f699632f8698cb2b7d83c2832ebba1601b1620
snoozleEmily <emilyalvimicm@gmail.com>
2025-10-12 20:01:08 -0300

    Removed warnings_config

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import warnings
-from src.logs.warnings_config import custom_warning
 from src.frontend.interface import Interface
 from src.utils.end_flow import EndFlow
 
 
-# Protect sensitive path info
-warnings.formatwarning = custom_warning
 
 def main():
     flow = EndFlow()
