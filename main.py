1326bf9dd6be8b4cddf006634b169a6c0a05aa64
snoozleEmily <emilyalvimicm@gmail.com>
2025-10-16 19:31:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from src.frontend.interface import Interface
 from src.utils.end_flow import EndFlow
 
 
-
 def main():
     flow = EndFlow()
     app = Interface(flow)
     app.mainloop()
+
 
 if __name__ == "__main__":
     main()
